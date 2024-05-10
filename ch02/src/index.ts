f0beb70ad40cee7840ad64ae4858470e2092c560
Steven Liu <stevenliu986@gmail.com>
2024-05-10 22:45:36 +1000

    update index.ts

--- ch02/src/index.ts
+++ ch02/src/index.ts
@@ -1,2 +1,18 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+
+let todos = [
+  new TodoItem(1, "Buy Flowers"),
+  new TodoItem(2, "Get Shoes"),
+  new TodoItem(3, "Collect Tickets"),
+  new TodoItem(4, "Call Joe", true),
+];
+
+let collection = new TodoCollection("Steven", todos);
+
 console.clear();
-console.log("Steven's Todo List");
+console.log(`${collection.userName}'s Todo List`);
+
+let newId = collection.addTodo("Go for run");
+let todoItem = collection.getTodoById(newId);
+todoItem.printDetails();
