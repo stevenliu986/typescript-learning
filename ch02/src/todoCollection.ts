956285d6ff2503dbe895aa26c41ab8f7f0b7a2d0
Steven Liu <stevenliu986@gmail.com>
2024-05-10 23:07:22 +1000

    update files

--- ch02/src/todoCollection.ts
+++ ch02/src/todoCollection.ts
@@ -1,25 +1,30 @@
-import {TodoItem} from "./todoItem";
+import { TodoItem } from "./todoItem";
 
 export class TodoCollection {
-    private nextId = 1;
+  private nextId = 1;
+  private itemMap = new Map<number, TodoItem>();
 
-    constructor(public userName: string, public todoItems: TodoItem[] = []) {
-    }
+  constructor(
+    public userName: string,
+    public todoItems: TodoItem[] = [],
+  ) {
+    todoItems.forEach((item) => this.itemMap.set(item.id, item));
+  }
 
-    addTodo(task: string): number {
-        while (this.getTodoById(this.nextId)) {
-            this.nextId++;
-        }
-        this.todoItems.push(new TodoItem(this.nextId, task));
-        return this.nextId;
+  addTodo(task: string): number {
+    while (this.getTodoById(this.nextId)) {
+      this.nextId++;
     }
-    getTodoById(id: number) : TodoItem {
-        return this.todoItems.find(item => item.id === id);
+    this.itemMap.set(this.nextId, new TodoItem(this.nextId, task));
+    return this.nextId;
+  }
+  getTodoById(id: number): TodoItem {
+    return this.itemMap.get(id);
+  }
+  markComplete(id: number, complete: boolean) {
+    const todoItem = this.getTodoById(id);
+    if (todoItem) {
+      todoItem.complete = complete;
     }
-    markComplete(id: number, complete: boolean) {
-        const todoItem = this.getTodoById(id);
-        if (todoItem) {
-            todoItem.complete = complete;
-        }
-    }
+  }
 }
