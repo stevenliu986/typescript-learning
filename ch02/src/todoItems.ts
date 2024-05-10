c7e8c0adcd0852b8723b1e952317d49684835d37
Steven Liu <stevenliu986@gmail.com>
2024-05-10 21:47:55 +1000

    add todoItems

---
+++ ch02/src/todoItems.ts
@@ -0,0 +1,16 @@
+export class TodoItems {
+    id: number;
+    task: string;
+    complete = false;
+
+    constructor(id: number, task: string, complete = false) {
+        this.id = id;
+        this.task = task;
+        this.complete = complete;
+    }
+
+    printDetails(): void {
+        console.log(`${this.id}\t${this.task} ${this.complete
+            ? "\t(complete)" : ""}`);
+    }
+}
