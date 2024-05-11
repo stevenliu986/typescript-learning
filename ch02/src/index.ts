9519a7d0bf439313a0bcb946e02e3482e6faf1d6
Steven Liu <stevenliu986@gmail.com>
2024-05-11 15:02:40 +1000

    update files

--- ch02/src/index.ts
+++ ch02/src/index.ts
@@ -11,7 +11,10 @@ let todos: TodoItem[] = [
 let collection: TodoCollection = new TodoCollection("Steven", todos);
 
 console.clear();
-console.log(`${collection.userName}'s Todo List`);
+console.log(
+  `${collection.userName}'s Todo List` +
+    `(${collection.getItemCounts().incomplete} items to do)`,
+);
 
 // let newId: number = collection.addTodo("Go for run");
 // let todoItem: TodoItem = collection.getTodoById(newId);
