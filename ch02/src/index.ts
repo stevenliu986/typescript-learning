31490285167ce1f8c99c1e84809ae9ac1a5691b7
Steven Liu <stevenliu986@gmail.com>
2024-05-11 15:37:38 +1000

    update file

--- ch02/src/index.ts
+++ ch02/src/index.ts
@@ -1,5 +1,6 @@
 import { TodoItem } from "./todoItem.js";
 import { TodoCollection } from "./todoCollection.js";
+import inquier from "inquirer";
 
 let todos: TodoItem[] = [
   new TodoItem(1, "Buy Flowers"),
@@ -10,14 +11,33 @@ let todos: TodoItem[] = [
 
 let collection: TodoCollection = new TodoCollection("Steven", todos);
 
-console.clear();
-console.log(
-  `${collection.userName}'s Todo List` +
-    `(${collection.getItemCounts().incomplete} items to do)`,
-);
+function displayTodoList(): void {
+  console.log(
+    `${collection.userName}'s Todo List` +
+      `(${collection.getItemCounts().incomplete} items to do)`,
+  );
+  collection.getTodoItems(true).forEach((item) => item.printDetails());
+}
 
-// let newId: number = collection.addTodo("Go for run");
-// let todoItem: TodoItem = collection.getTodoById(newId);
-// todoItem.printDetails();
-collection.removeComplete();
-collection.getTodoItems(true).forEach((item) => item.printDetails());
+enum Commands {
+  Quit = "Quit",
+}
+
+function promptUser(): void {
+  console.clear();
+  displayTodoList();
+  inquier
+    .prompt({
+      type: "list",
+      name: "command",
+      message: "Choose option",
+      choices: Object.values(Commands),
+    })
+    .then((answers) => {
+      if (answers["command"] !== Commands.Quit) {
+        promptUser();
+      }
+    });
+}
+
+promptUser();
