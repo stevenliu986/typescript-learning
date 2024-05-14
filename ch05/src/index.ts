77a12410b96d3407bfc53f16f8d40011ee18f7eb
StevenLiu <stevenliu986@gmail.com>
2024-05-14 22:19:23 +1000

    update file

--- ch05/src/index.ts
+++ ch05/src/index.ts
@@ -1,5 +1,9 @@
+import {sum} from "./calc";
+
 function printMessage(msg: string): void {
     console.log(`Message: ${msg}`);
 }
 
 printMessage("Hello, TypeScript");
+let total = sum(100,200,300);
+console.log(total);
